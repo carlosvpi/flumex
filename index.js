0689db63c1c7ef33694950783757c3f2ff396e63
CarlosVazPI <carlos.vazpi@gmail.com>
2019-07-11 08:54:26 +0100

    Reshaped methods

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const noop = () => {}
 
-function Stream (init) {
-    this.value = init
+function Flumex (generator, cancel = noop) {
+    this.value
     this.done = false
-    this.cancel = noop
     this.closing = false
     this.subscriptions = []
+    const set = (value) => {
+        if (this.done) {
+            return
+        }
+        if (this.closing) {
+            this.done = true
+            cancel(set)
+            this.closing = false
+        }
+        this.value = value
+        this.subscriptions.forEach((resolve) => {
+            resolve({ value: this.value, done: this.done })
+        })
+        this.subscriptions = []
+    }
+    generator(set, () => this.closing = true)
 }
 
-Stream.prototype.next = function () {
+Flumex.prototype.next = function () {
     return new Promise((resolve) => {
         if (!this.subscriptions.includes(resolve)) {
             this.subscriptions.push(resolve)
@@ -16,74 +31,235 @@ Stream.prototype.next = function () {
     })
 }
 
-Stream.prototype.set = function (value) {
-    if (this.done) {
-        return
-    }
-    if (this.closing) {
-        this.done = true
-        this.cancel()
-        this.closing = false
-    }
-    this.value = value
-    this.subscriptions.forEach((resolve) => {
-        resolve({ value: this.value, done: this.done })
-    })
-    this.subscriptions = []
-}
-
-Stream.prototype.close = function (value) {
+Flumex.prototype.close = function (value) {
     this.closing = true
 }
 
-Stream.fromInterval = (interval) => {
-    const s = new Stream()
+Flumex.prototype.forEach = function (f) {
+    return Flumex.forEach(f)(this)
+}
+Flumex.forEach = (f) => async (flumex) => {
+    while (await flumex.next() && !flumex.done) {
+        f(flumex)
+    }
+    f(flumex)
+}
+
+Flumex.prototype.map = function (f) {
+    return Flumex.map(f)(this)
+}
+Flumex.map = (f) => (flumex) => {
+    return new Flumex((set, close) => {
+        flumex.forEach(({ value, done }) => {
+            if (done) {
+                close(f(value))
+            } else {
+                set(f(value))
+            }
+        })
+    })
+}
+
+Flumex.prototype.filter = function (p) {
+    return Flumex.filter(p)(this)
+}
+Flumex.filter = (p) => (flumex) => {
+    return new Flumex((set, close) => {
+        flumex.forEach(({ value, done }) => {
+            if (done) {
+                close(p(value) ? value : undefined)
+            } else if (p(value)) {
+                set(value)
+            }
+        })
+    })
+}
+
+Flumex.prototype.concat = function (...flumexes) {
+    return Flumex.concat(this, ...flumexes)
+}
+Flumex.concat = (...flumexes) => {
+    const subscribe = (index) => {
+        const isLast = index === flumexes - 1
+
+        return (set, close) => {
+            flumexes[index].forEach(({ value, done }) => {
+                if (done) {
+                    if (isLast) {
+                        close(value)
+                    } else {
+                        subscribe(index + 1)(set, close)
+                    }
+                } else {
+                    set(value)
+                }
+            })
+        }
+    }
+    return new Flumex(subscribe(0))
+}
+
+Flumex.prototype.compact = function () {
+    return Flumex.compact(this)
+}
+Flumex.compact = (() => {
+    const nil = (x) => x === null || x === undefined
+    return () => Flumex.reject(nil)
+})()
+
+Flumex.prototype.reject = function (p) {
+    return Flumex.reject(p)(this)
+}
+Flumex.reject = (p) => (flumex) => {
+    return new Flumex((set, close) => {
+        flumex.forEach(({ value, done }) => {
+            if (done) {
+                close(!p(value) ? value : undefined)
+            } else if (!p(value)) {
+                set(value)
+            }
+        })
+    })
+}
+
+Flumex.prototype.reduce = function (f, init) {
+    return Flumex.reduce(f, init)(this)
+}
+Flumex.reduce = (f, init) => (flumex) => {
+    return new Flumex((set, close) => {
+        let acc
+        flumex.forEach(({ value, done }) => {
+            acc = f(acc, value)
+            if (done) {
+                close(acc)
+            } else {
+                set(acc)
+            }
+        })
+    })
+}
+Flumex.prototype.mergeRace = function (...flumexes) {
+    return Flumex.mergeRace(this, ...flumexes)
+}
+Flumex.mergeRace = (...flumexes) => {
+    return new Flumex((set, close) => {
+        flumexes.forEach((flumex) => {
+            flumex.forEach(({ value, done }) => {
+                if (done) {
+                    close(value)
+                } else {
+                    set(value)
+                }
+            })
+        })
+    })
+}
+Flumex.prototype.mergeAll = function (...flumexes) {
+    return Flumex.mergeAll(this, ...flumexes)
+}
+Flumex.mergeAll = (...flumexes) => {
+    return new Flumex((set, close) => {
+        flumexes.forEach((flumex) => {
+            flumex.forEach(({ value, done }) => {
+                const areAllDone = flumexes.reduce((acc, { done }) => acc && done, true)
+                if (areAllDone) {
+                    close(value)
+                } else {
+                    set(value)
+                }
+            })
+        })
+    })
+}
+
+Flumex.every = (...flumexes) => {
+    return new Flumex((set, close) => {
+        flumexes.forEach((flumex) => {
+            flumex.forEach(({ value, done }) => {
+                const allValues = flumexes.map(({ value }) => value)
+                const allDone = flumexes.reduce((acc, { done }) => acc && done, true)
+                if (allDone) {
+                    close(allValues)
+                } else {
+                    set(allValues)
+                }
+            })
+        })
+    })
+}
+
+Flumex.some = (...flumexes) => {
+    return new Flumex((set, close) => {
+        flumexes.forEach((flumex) => {
+            flumex.forEach(({ value, done }) => {
+                const allValues = flumexes.map(({ value }) => value)
+                const someDone = flumexes.reduce((acc, { done }) => acc || done, false)
+                if (someDone) {
+                    close(allValues)
+                } else {
+                    set(allValues)
+                }
+            })
+        })
+    })
+}
+
+Flumex.fromInterval = (interval, cleanAfter = 10000) => {
     let i = 0
-    const token = setInterval(() => {
-        s.set(i++)
-    }, interval)
-    s.cancel = () => clearInterval(token)
-    setTimeout(s.cancel, 10000)
+    let token
+    const s = new Flumex((set) => {
+        token = setInterval(() => {
+            set(i++)
+        }, interval)
+    }, () => clearInterval(token))
+    cleanAfter && setTimeout(s.cancel, cleanAfter)
     return s
 }
 
-Stream.prototype.forEach = async function (f) {
-    while (await this.next() && !this.done) {
-        f(this)
-    }
-    f(this)
+Flumex.fromFunction = (f) => {
+    let setFlumex, closeFlumex
+    const flumex = new Flumex((set, close) => {
+        setFlumex = set
+        closeFlumex = close
+    })
+    return [flumex, setFlumex, closeFlumex]
 }
 
-Stream.prototype.map = function (f) {
-    const s = new Stream()
-    this.forEach(({ value, done }) => {
-        debugger
-        if (done) {
-            s.close(f(value))
-        } else {
-            s.set(f(value))
+Flumex.fromEvent = (node, eventName, useCapture) => {
+    return new Flumex((set) => {
+        node.addEventListener(eventName, set, useCapture)
+    }, (set) => {
+        node.removeEventListener(eventName, set, useCapture)
+    })
+}
+
+Flumex.fromFetch = (url) => {
+    let fetcher
+    const flumex = new Flumex((set) => {
+        fetcher = async (options) => {
+            set(await fetch(url, options))
         }
     })
-    return s
+    return [flumex, fetcher]
 }
 
-f = async (stream) => {
+f = async (flumex) => {
     let value, done
     do {
-        const next = await stream.next()
+        const next = await flumex.next()
         value = next.value
         done = next.done
         if (value >= 3) {
-            stream.close()
+            flumex.close()
         }
         console.log(value)
     } while (!done)
 }
 
-g = async (stream) => {
+g = async (flumex) => {
     let value, done
     do {
-        const next = await stream.next()
+        const next = await flumex.next()
         value = next.value
         done = next.done
         console.log('party continues')
@@ -91,7 +267,6 @@ g = async (stream) => {
     console.log('Party has finished!! :(')
 }
 
-s = Stream.fromInterval(1000)
-// f(s)
-// g(s)
-f(Stream.fromInterval(1000).map(x => x * 2))
+s = Flumex.fromInterval(1000).map(x => x * 2)
+f(s)
+g(s)
